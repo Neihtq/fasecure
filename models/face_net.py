3296cd35cab5358fe50b480467793cc480c574b5
neihtfool <q.thien.nguyen@outlook.de>
2021-01-04 18:11:19 +0100

    implement metric with evaluation method for embedding

--- models/face_net.py
+++ models/face_net.py
@@ -2,7 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
+import numpy as np
 
+from pytorch_lightning.metrics import Metric
 from torchvision.models import resnet50
 
 
@@ -43,7 +45,6 @@ class FaceNet(nn.Module):
             nn.Linear(2048*8*8, embedding_size))
         
         self.model.classifier = nn.Linear(embedding_size, num_classes)
-        self.criterion = nn.TripletMarginLoss(margin=self.hparams["margin"],p=2)
 
     def l2_norm(self, input):
         input_size = input.size()
@@ -112,90 +113,108 @@ class LightningFaceNet(pl.LightningModule):
     def __init__(self, hparams, pretrained=False):
         super(LightningFaceNet, self).__init__()
         self.model = FaceNet(hparams, pretrained=pretrained)
-        
+        self.criterion = nn.TripletMarginLoss(margin=hparams["margin"], p=2)
+        self.train_metric = EmbeddingAccuracy()
+        self.val_metric = EmbeddingAccuracy()
+        self.test_metric = EmbeddingAccuracy()
+
     def forward(self, x):
         return self.model(x)
         
-    def general_step(self, batch):
+    def general_step(self, batch, mode):
         label, anchor, positive, negative = batch
         
         anchor_enc = self.forward(anchor)
         pos_enc = self.forward(positive)
         neg_enc = self.forward(negative)
         
-        loss = self.model.criterion(anchor_enc, pos_enc, neg_enc)
+        loss = self.criterion(anchor_enc, pos_enc, neg_enc)
+
+        if mode == 'train':
+            self.train_metric(anchor_enc, pos_enc, neg_enc)
+        elif mode == 'val':
+            self.val_metric(anchor_enc, pos_enc, neg_enc)
+        else:
+            self.test_metric(anchor_enc, pos_enc, neg_enc)
 
         return loss
     
     def training_step(self, batch, batch_idx):
-        loss = self.general_step(batch)
+        loss = self.general_step(batch, "train")
         log = {'train_loss': loss}
-        
+
+        accuracy, precision, recall, f1_score = self.train_metric.compute()
+        self.log("train_acc", accuracy, prog_bar=True, logger=True)
+
         return {"loss": loss, "log": log}
-        
+
+    def training_epoch_end(self, training_step_outputs):
+        accuracy, precision, recall, f1_score = self.train_metric.compute()
+        self.log("train_epoch_acc", accuracy, prog_bar=True, logger=True)
+
     def validation_step(self, batch, batch_idx):
-        loss = self.general_step(batch)
+        loss = self.general_step(batch, "val")
         log = {'val_loss': loss}
-        self.log('val_loss', loss)
-        
-        accuracy, precision, recall, F1_Score = self.evaluation(batch)
-        
-        print("Validation Accuracy: " + accuracy)
-        print("Validation Precision: " + precision)
-        print("Validation Recall: " + recall)
-        print("Validation F1_Score: " + F1_Score)
-        
-        return {"val_loss": loss, "log": log, "val_acc": accuracy, "val_precision": precision, "val_recall": recall, "val_F1": F1_Score} 
-        
-    def evaluation(self, batch):
-        for x in batch:
-            #img_1 = load_and_transform_img(x)
-            label, anchor, positive, negative = batch
-            anchor_enc = self.forward(anchor)
-            pos_enc = self.forward(positive)
-            neg_enc = self.forward(negative)  
-            
-            anchor_enc_np = anchor_enc.numpy()
-            pos_enc_np = pos_enc.numpy()
-            neg_enc_np = neg_enc.numpy()
-            
-            d_a_p = numpy.linalg.norm(anchor_enc_np - pos_enc_np)
-            d_a_n = numpy.linalg.norm(anchor_enc_np - neg_enc_np)
-            
-            threshold = 0.02
-            
-            FN, TP, FP, TN = 0, 0, 0, 0
-            if d_a_p > threshold:
-                FN += 1
-            else:
-                TP += 1
-                
-            if d_a_n <= threshold:
-                FP += 1
-            else:
-                TN += 1
-            
-        accuracy = TP + TN / (TP + TN + FP + FN)
-        precision = TP / (TP + FP)
-        recall = TP / (TP + FN)
-        F1_Score = TP / (TP + 0.5 * (FP + FN)) 
-        
-        return accuracy, precision, recall, F1_Score
-    
+        self.log("val_loss", loss, logger=True)
+
+        return {"val_loss": loss, "log": log}
+
+    def validation_epoch_end(self, validation_step_outputs):
+        accuracy, precision, recall, f1_score = self.val_metric.compute()
+        self.log("val_acc", accuracy, logger=True)
+
     def test_step(self, batch, batch_idx):
-        loss = self.general_step(batch)
+        loss = self.general_step(batch, "test")
         log = {'test_loss': loss}
+        self.log("test_loss", loss, prog_bar=True)
+        self.log("test_acc", self.test_metric.compute(), prog_bar=True)
         
-        print("Test Accuracy: " + accuracy)
-        print("Test Precision: " + precision)
-        print("Test Recall: " + recall)
-        print("Test F1_Score: " + F1_Score)
+        return {"test_loss": loss, "log": log}
         
-        return {"test_loss": loss, "log": log, "test_acc": accuracy, "test_precision": precision, "test_recall": recall, "test_F1": F1_Score}
-        
+    def test_epoch_end(self, test_step_outputs):
+        accuracy, precision, recall, f1_score = self.test_metric.compute()
+        self.log("test_acc", accuracy, prog_bar=True, logger=True)
+
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.model.parameters(), lr=self.model.hparams['lr'], weight_decay=1e-5)
         if self.model.hparams['optimizer'] == 'SGD':
             optimizer = torch.optim.SGD(self.model.parameters(), lr=self.model.hparams['lr'], weight_decay=0.0001)
                     
         return optimizer
+
+
+class EmbeddingAccuracy(Metric):
+    def __init__(self, threshold=0.2):
+        super().__init__()
+        self.threshold = threshold
+        self.add_state("false_positive", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("true_positive", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("true_negative", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("false_negative", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, anchor_enc, pos_enc, neg_enc):
+        assert anchor_enc.shape == pos_enc.shape == neg_enc.shape
+        l2_pos = torch.dist(anchor_enc, pos_enc, 2)
+        l2_neg = torch.dist(anchor_enc, neg_enc, 2)
+
+        FN, TP, FP, TN = 0, 0, 0, 0
+        if l2_pos > self.threshold:
+            self.false_negative += 1
+        else:
+            self.true_negative += 1
+            
+        if l2_neg <= self.threshold:
+            self.false_positive += 1
+        else:
+            self.true_positive += 1
+                
+    def compute(self):
+        TP, TN = self.true_positive, self.true_negative
+        FP, FN = self.false_positive, self.false_negative        
+
+        accuracy = TP + TN / (TP + TN + FP + FN)
+        precision = TP / (TP + FP)
+        recall = TP / (TP + FN)
+        F1_Score = TP / (TP + 0.5 * (FP + FN)) 
+        
+        return accuracy, precision, recall, F1_Score
