bb0e28d8ae14a9da225fc8aed99e43e0a3541a8c
Zeulni <tobias-zeulner@gmx.de>
2021-01-08 15:59:32 +0100

    merged overall evaluation into main

--- pipeline_evaluation.py
+++ pipeline_evaluation.py
@@ -14,34 +14,13 @@ from utils.prep import load_and_transform_img
 from evaluation.PipelineEvaluation import PipelineEvaluation
 
 # to Cao, Simon and Thien: model.eval()!
-# main results:
-# - euclidean distance or inner product no difference
-# - data augmentation so far no difference (-> look at images)
-# - works bad for less persons (-> paper used fixed threshold as initial value of adaptive threshold procedure)
-# - works a bit better for few-shot learning instead of one-shot learning (currently 3 images per person)
-
-# Other similarities:
-# - Manhatten Distance
-# - Cosine Similarity
-
-# main results 2:
-# - Created evaluation for whole pipeline
-# - only use 1/10 of lfw for evaluation so far and only if at least 6 images per folder 
-#       (->540 images for overal evaluation)
-#       -> face recognition is more often tested with already registered user, so can test one-shot learning more often
-
-# - log the evaluation and plot results
-# - can choose wheter to use detection model or directly cropped images
-#   (only in evaluation, if face detected. Otherwise ignored)
-# - Implemented reference face embedding model which we have to beat (lower bound)
 
 # todo
-# - Use whole LFW dataset (when class members > 5)
 # - compare to fixed_threshold = 0 (normal case? and print also the first cases, as there very bad?)
 # - implement code, that if cuda available, if moves everything on the gpu
 # - evalation normally on which dataset? new one?
-# - have to find optimal fixed_threshold as lower bound (and initial threshold) to fix problem with too less registered people
 # - run with that threshold reference model (or with other threshold?)
+# - others should train embedding model with same augmentation techniques as I use for registration!!
 # - compare accuracy with paper
 
 # from deepface import DeepFace
@@ -61,13 +40,17 @@ def evaluate_pipeline():
         os.makedirs(join(absolute_dir, "evaluation", "evaluation_results"))
 
     # loop over different fixed thresholds to find the one resulting in the highest accuracy
-    for fixed_threshold in range(0,100,1):
+    thresholds = [98.5]
+    # best threshold 98.5 (compare_num 530: acc: 0.6509)
+    for fixed_threshold in thresholds:
         eval_log_path_fix = eval_log_path + str(fixed_threshold) + ".txt"
         registration_database = RegistrationDatabase(fixed_threshold=fixed_threshold)
         pipeline_evaluation = PipelineEvaluation(dataset_path, eval_log_path_fix,
                                             face_embedding_model, registration_database)
-        pipeline_evaluation.run()
-    pipeline_evaluation.plot_results()
+        #pipeline_evaluation.run()
+        pipeline_evaluation.plot_results()
+        #pipeline_evaluation.compare_evaluations()
+        # sys.exit()
 
 
 if __name__ == '__main__':
