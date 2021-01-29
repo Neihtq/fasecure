f8b8f150f864288390c437519cfff0a25c906665
neihtfool <q.thien.nguyen@outlook.de>
2021-01-29 01:29:09 +0100

    new file structure

--- face_recognition/evaluate.py
+++
@@ -1,7 +0,0 @@
-from face_recognition.evaluation.evaluations import evaluate_pipeline
-
-from constants import LFW_CROP_DIR, EVAL_RESULTS_DIR
-
-
-if __name__ == '__main__':
-    evaluate_pipeline(LFW_CROP_DIR, EVAL_RESULTS_DIR)
