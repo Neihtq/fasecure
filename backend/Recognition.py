228b0de3ff099df971893db26ca17110b20fcf20
neihtfool <q.thien.nguyen@outlook.de>
2021-02-03 01:35:27 +0100

    use pretrained model

--- backend/Recognition.py
+++ backend/Recognition.py
@@ -10,8 +10,7 @@ from face_recognition.utils.data_augmentation import augment_and_normalize
 
 class Recognition:
     def __init__(self):
-        self.model = get_model()
-        #self.model = load_weights()
+        self.model = load_weights()
         self.model.eval()
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model.to(self.device)
